135390660139a6991ae021745c764163675dd2a3
Victor <vicky.robilson55@gmail.com>
2024-08-13 17:47:08 -0700

    remove error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,22 +7,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """ 
-    Return the total number of subscribers in a given Reddit subreddit.
-    
+    """
+    Return the total number of subscribers in a given Reddit subreddit
     Args:
         subreddit (str): The name of the subreddit to query.
     """
-
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/vikkycode_)"
     }
-
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 404:
         return 0
-    
         results = response.json().get("data")
         return results.get("subscribers")
