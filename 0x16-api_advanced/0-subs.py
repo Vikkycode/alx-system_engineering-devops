b1a315a0450d5c75ffa96ef52ef1bd341d42cbe7
Victor <vicky.robilson55@gmail.com>
2024-08-13 17:00:24 -0700

    aremoved space

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,12 +2,13 @@
 """Function to query subscribers on a given reddit subreedit"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ Return the total number of subscribers on a given reddit subreddit"""
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-            'User-Agent':"linux:0x16.api.advanced:v1.0.0 (by /u/vikkycode_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/vikkycode_)"
     }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -15,5 +16,3 @@ def number_of_subscribers(subreddit):
         return 0
     results = response.json().get("data")
     return results.get("subscribers")
-
-
