402c2a9dc7c67fbdd413bee6173361d25676fd2e
Victor <vicky.robilson55@gmail.com>
2024-08-13 17:38:07 -0700

    correct it

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-Function to query subscribers on a given reddit subreedit
+Function to query the number of subscribers in a given Reddit subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-
-    """
+    """ 
     Return the total number of subscribers in a given Reddit subreddit.
-
+    
     Args:
         subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers. Returns 0 if the subreddit is invalid or on error.
     """
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
@@ -20,7 +23,13 @@ def number_of_subscribers(subreddit):
     }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers", 0)
+    
+    try:
+        results = response.json().get("data")
+        return results.get("subscribers", 0)
+    except (ValueError, AttributeError):
+        return 0
+
