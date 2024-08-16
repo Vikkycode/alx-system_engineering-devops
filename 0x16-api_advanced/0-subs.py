e0d6c7b49096ed226917751e77b75e8bf8297c1f
Victor <vicky.robilson55@gmail.com>
2024-08-15 21:26:40 -0700

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,16 +8,23 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Return the total number of subscribers in a given Reddit subreddit
+    Return the total number of subscribers in a given Reddit subreddit.
+    
     Args:
         subreddit (str): The name of the subreddit to query.
+        
+    Returns:
+        int: The number of subscribers or 0 if the subreddit doesn't exist.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/vikkycode_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            results = response.json().get("data", {})
+            return results.get("subscribers", 0)
         return 0
-        results = response.json().get("data")
-        return results.get("subscribers")
+    except requests.RequestException:
+        return 0
